6736d3d4b043093d86a6bf0f75167ce45819563c
Brant McCarthy <brant.mccarthy@me.com>
2022-03-14 11:36:22 -0500

    added comments in pokemon.router on how to generate your own api key

--- server/routes/pokemon.router.js
+++ server/routes/pokemon.router.js
@@ -3,27 +3,15 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 
-// router.get('/', (req, res) => {
-//     console.log('req.body:', req.body);
-//     console.log('req.data:', req.data);
-    
-//     axios.get(`https://api.pokemontcg.io/v2/cards?X-Api-Key=${process.env.POKEMON_API_KEY}&q=name:charizard&page=1&pageSize=50&orderBy=set.releaseDate`)
-    
-//         .then(response => {
-//             console.log('in /pokemon/api .get');
-//             res.send(response.data)
-//         }).catch(err => {
-//             console.log('ERROR in GET', err);
-//             res.sendStatus(500);
-//         })
-// })
-
 router.post('/', (req, res) => {
     console.log('req.body:', req.body.query);
     const searchQuery = req.body.query
     const setId = req.body.setId
     console.log('req.body.set', req.body.set);
-    
+
+    // TO GET IT TO WORK:
+    // 1 - Register FREE api key at: https://dev.pokemontcg.io/
+    // 2 - Add .env file to root folder with your specific API key
     
     axios.get(`https://api.pokemontcg.io/v2/cards?X-Api-Key=${process.env.POKEMON_API_KEY}&q=name:*${searchQuery}* set.id:${setId}&page=1&pageSize=20&orderBy=set.releaseDate`)
     
